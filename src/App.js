efaad004c0c9ef5cb191ca402e6b96f2fabf3a3f
Maha <mahamustafa.m2@gmail.com>
2020-09-11 12:39:44 +0500

    added a new component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import CardItem from './components/cards/CardItem';
 import cardData from './components/cards/cardData';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
+import Showcase from './components/showcase/Showcase';
 
 function App() {
   const cardItems = cardData.map((item)=>(
@@ -12,6 +13,7 @@ function App() {
   return (
     <>
       <Navbar />
+      <Showcase/>
       <div className="App">
         {/* <CardItem/> */}
         <div className="info-card">{cardItems}</div>
---
+++ src/components/showcase/Showcase.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Showcase() {
+    return (
+        <div id="showcase">
+            <h1 className="showcase-text">Welcome!</h1>
+            <p className="showcase-text">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi officiis ipsum officia numquam expedita ullam.
+            </p>
+            <a href="#" class="showcase-button">Read More</a>
+        </div>
+    )
+}
